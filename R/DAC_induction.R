8e8aabf3ee151ecbbf8e8fa53ee8681a32cdbaa1
Julie Devis <julie.devis@uclouvain.be>
2022-12-21 16:42:40 +0100

    Review DAC_induction()

--- R/DAC_induction.R
+++ R/DAC_induction.R
@@ -1,6 +1,6 @@
 #' Gene expression in cells treated or not by a demethylating agent
 #'
-#' @description Plots a heatmap of gene normalised counts (log-transformed)
+#' @description Plots a heatmap of normalised gene counts (log-transformed)
 #' in a selection of cells treated or not by 5-Aza-2′-Deoxycytidine (DAC),
 #' a demethylating agent.
 #'
@@ -45,9 +45,10 @@ DAC_induction <- function(database, genes = NULL) {
   }
 
   if (!is.null(genes)) {
-    if (!all(genes %in% rowData(data)$external_gene_name)) { ## Warning !
+    if (!all(genes %in% rowData(data)$external_gene_name)) {
       message("Check gene name(s)!\n")
-      message(paste0(genes[!genes %in% rowData(data)$external_gene_name], " is not in the database.\n"))
+      message(paste0(genes[!genes %in% rowData(data)$external_gene_name],
+                     " is not in the database.\n"))
       genes <- genes[genes %in% rowData(data)$external_gene_name]
       stopifnot(!is_empty(genes))
     }
@@ -67,27 +68,29 @@ DAC_induction <- function(database, genes = NULL) {
   rownames(df_col) <- rownames(colData(data))
   df_col <- df_col[order(df_col$cell, df_col$treatment), ]
 
-  column_ha_cell <- HeatmapAnnotation(
-    cell = df_col$cell,
-    border = TRUE)
+  column_ha_cell <- HeatmapAnnotation(cell = df_col$cell,border = TRUE)
 
   column_ha_treatment <- HeatmapAnnotation(
     treatment = df_col$treatment,
     col = list(treatment = c("CTL" = "cyan", "DAC" = "firebrick1")),
     border = TRUE)
 
-  if (dim(mat)[1] > 100){ fontsize <- 4 }
-  if (dim(mat)[1] > 50 & dim(mat)[1] <= 100){ fontsize <- 5 }
-  if (dim(mat)[1] > 20 & dim(mat)[1] <= 50){ fontsize <- 6 }
+  if (dim(mat)[1] > 100) { fontsize <- 4 }
+  if (dim(mat)[1] > 50 & dim(mat)[1] <= 100) { fontsize <- 5 }
+  if (dim(mat)[1] > 20 & dim(mat)[1] <= 50) { fontsize <- 6 }
   if (dim(mat)[1] <= 20) { fontsize <- 8 }
 
   h <- suppressMessages(Heatmap(mat[, rownames(df_col), drop = FALSE],
                                 name = "logCounts",
                                 column_split = factor(df_col$cell),
                                 col = colorRamp2(seq(0, max(mat), length = 11),
-                                                 c("#5E4FA2", "#3288BD", "#66C2A5", "#ABDDA4",
-                                                   "#E6F598", "#FFFFBF", "#FEE08B", "#FDAE61",
-                                                   "#F46D43", "#D53E4F", "#9E0142")),
+                                                 c("#5E4FA2", "#3288BD",
+                                                   "#66C2A5", "#ABDDA4",
+                                                   "#E6F598", "#FFFFBF",
+                                                   "#FEE08B", "#FDAE61",
+                                                   "#F46D43", "#D53E4F",
+                                                   "#9E0142")),
+
                                 cluster_rows = TRUE,
                                 show_row_dend = FALSE,
                                 clustering_method_rows = "ward.D",
@@ -95,7 +98,9 @@ DAC_induction <- function(database, genes = NULL) {
                                 cluster_columns = FALSE,
                                 row_names_gp = gpar(fontsize = fontsize),
                                 column_title_gp = gpar(fontsize = 0),
-                                top_annotation = c(column_ha_cell, column_ha_treatment)))
+                                top_annotation = c(column_ha_cell,
+                                                   column_ha_treatment)))
   print(h)
   invisible(mat)
 }
+
