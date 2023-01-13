32855666367867ae9a6b10bcbef095be169b70ce
aloriot <axelle.loriot@uclouvain.be>
2023-01-13 14:29:17 +0100

    updated TCGA_methylation_expression_correlation.R

--- R/TCGA_methylation_expression_correlation.R
+++ R/TCGA_methylation_expression_correlation.R
@@ -3,10 +3,16 @@
 #' @description Plots the correlation between methylation and
 #' expression values of a Cancer-Testis (CT) gene in TCGA samples.
 #'
-#' @param gene CT gene name(s)
+#' @param gene CT gene name
 #'
 #' @param tumor TCGA tumor code. c("SKCM", "LUAD", "LUSC", "COAD", "ESCA",
 #' "BRCA", "HNSC", "all")
+#' 
+#' @param nt_up Number of nucleotides upstream the TSS to define the promoter 
+#' region (1000 by default)
+#'
+#' @param nt_down Number of nucleotides downstream the TSS to define the 
+#' promoter region (200 by default)
 #'
 #' @param corr_coeff Boolean (FALSE by default). If set to TRUE, the function
 #' will invisibly return the correlation coefficient (Pearson), between
@@ -33,169 +39,22 @@
 #' @importFrom SummarizedExperiment assay colData rowData
 #' @importFrom GenomicRanges makeGRangesFromDataFrame
 #' @importFrom IRanges subsetByOverlaps
-#' @importFrom tibble tibble enframe
-#' @importFrom dplyr left_join select mutate
+#' @importFrom tibble tibble
+#' @importFrom dplyr filter left_join select mutate
 #' @importFrom ggplot2 ggplot aes geom_point geom_smooth ggtitle xlim
 #' @importFrom stats cor.test
 #'
 #' @examples
-#' TCGA_methylation_expression_correlation(tumor = "LUAD", gene = "TDRD1")
-#' TCGA_methylation_expression_correlation(tumor = "all", gene = "TDRD1")
-#' TCGA_methylation_expression_correlation(tumor = "all", gene = "TDRD1",
-#' corr_coeff = TRUE)
-# TCGA_methylation_expression_correlation <- function(tumor, gene = NULL,
-#                                                     return = FALSE,
-#                                                     corr_coeff = FALSE) {
-# 
-#   if (missing(tumor)) {
-#     stop("TCGA tumor code ('SKCM', 'LUAD', 'LUSC', 'COAD', 'ESCA', 'BRCA', 'HNSC' or 'all') must be specified!")
-#   }
-# 
-#   if (!all(tumor %in% c("SKCM", "LUAD", "LUSC", "COAD", "ESCA", "BRCA",
-#                         "HNSC", "all"))) {
-#     stop("TCGA tumor code must be one of ('SKCM', 'LUAD', 'LUSC', 'COAD', 'ESCA', 'BRCA', 'HNSC', or 'all')!")
-#   }
-# 
-#   if (is.null(gene)) {
-#     stop("Gene name must be specified!")
-#   }
-# 
-#   if (length(tumor) == 1 & tumor[1] == "all") {
-#     tumor <- c('SKCM', 'LUAD', 'LUSC', 'COAD', 'ESCA', 'BRCA', 'HNSC')
-#   }
-# 
-#     TPM <- TCGA_TPM
-#     met <- TCGA_CT_methylation
-# 
-#   if (!gene %in% CT_genes$external_gene_name) { ## Warning for length(gene) > 1
-#     message(paste0("Check gene name! ", gene, " is not in the database"))
-#     return(invisible(NA))
-#   }
-# 
-#   ## Load expression and methylation data from selected tumors
-#   ##(keeping only primary and metastatic tumors)
-#   projects <- paste0("TCGA-", tumor)
-#   TPM <- TPM[, colData(TPM)$project_id %in% projects]
-#   TPM <- TPM[, colData(TPM)$shortLetterCode == "TP" |
-#                colData(TPM)$shortLetterCode == "TM" |
-#                colData(TPM)$shortLetterCode == "NT"]
-# 
-#   met <- met[, colData(met)$project_id %in% projects]
-# 
-#   ## select tumors for which expression and methylation data are available
-#   samples <- intersect(colData(TPM)$sample, colData(met)$sample)
-# 
-#   ## Create a Grange corresponding to promoter regions
-#   nt_up <- 1000
-#   nt_down <- 200
-#   CT_promoter_gr <- makeGRangesFromDataFrame(
-#     CT_genes %>%
-#       dplyr::select(ensembl_gene_id, external_gene_name,
-#                     external_transcript_name,chromosome_name, strand,
-#                     transcription_start_site) %>%
-#       mutate(chromosome_name = paste0("chr", chromosome_name)) %>%
-#       mutate(strand = ifelse(strand == 1, '+', '-')) %>%
-#       mutate(start = case_when(strand == '+' ~ transcription_start_site - nt_up,
-#                                strand == '-' ~ transcription_start_site - nt_down)) %>%
-#       mutate(stop = case_when(strand == '+' ~ transcription_start_site + nt_down,
-#                               strand == '-' ~ transcription_start_site + nt_up)),
-#     keep.extra.columns = TRUE,
-#     seqnames.field = "chromosome_name",
-#     start.field = "start",
-#     end.field = "stop")
-# 
-#   met_roi <- subsetByOverlaps(met[, colData(met)$sample %in% samples],
-#                               CT_promoter_gr[CT_promoter_gr$external_gene_name == gene])
-# 
-#   ## Evaluate mean methylation value of promoter probe(s) in each sample
-#   met_mean <- colMeans(assay(met_roi), na.rm = TRUE)
-#   names(met_mean) <- substr(names(met_mean), 1, 16)
-# 
-#   ensembl <- CT_genes[CT_genes$external_gene_name == gene, "ensembl_gene_id",
-#                       drop = TRUE]
-#   TPM <- assay(TPM[rownames(TPM) %in% ensembl,
-#                    colData(TPM)$sample %in% samples])
-# 
-#   colnames(TPM) <- substr(colnames(TPM), 1, 16)
-# 
-#   if (nrow(TPM) == 0) {
-#     print(paste0(gene, " is not in TCGA expression database"))
-#     return(invisible(NA))
-#   }
-# 
-#   suppressMessages(
-#     methylation_expression <- left_join(enframe(met_mean, name = "sample",
-#                                                 value = "met"),
-#                                         enframe(TPM[1,], name = "sample",
-#                                                 value = "TPM"))
-#   )
-# 
-#   suppressMessages(
-#     methylation_expression <- methylation_expression %>%
-#     left_join(as_tibble(colData(met)) %>%
-#                 mutate(Tumor = sub(pattern = "TCGA-", x = project_id,
-#                                    replacement = '')) %>%
-#                 dplyr::select(sample, Tumor))
-#   )
-# 
-#   ## stop if no probes or no methylation values for probes within the region
-#   if (all(is.na(methylation_expression$met))) {
-#     message(paste0("No probes for ", gene))
-#     return(invisible(NA))
-#   }
-# 
-#   ## Gene has to be expressed (TPM >= 1) in at least 1%
-#   ## of the samples to evaluate correlation
-#   if (length(methylation_expression$TPM[methylation_expression$TPM] >= 1) <
-#       ceiling(nrow(methylation_expression) * 0.01)) {
-#     message(paste0("Too few positive samples to estimate a correlation for ",
-#                    gene))
-#     cor <- NA
-#   } else {
-#     cor <- cor.test(methylation_expression$met,
-#                     log1p(methylation_expression$TPM))$estimate
-#   }
-# 
-#   TPM <- met <- NULL
-# 
-#   methylation_expression$Tissue <- ifelse(substr(methylation_expression$sample,
-#                                                  14, 15) == "11",
-#                                           "Peritumoral", "Tumor")
-# 
-#   if (length(unique(methylation_expression$Tumor)) > 1) {  # color by tumor type
-#     p <- ggplot(methylation_expression[order(methylation_expression$Tissue,
-#                                              decreasing = TRUE), ],
-#                 aes(x = met, y = log1p(TPM))) +
-#       geom_point(alpha = 0.6, aes(color = Tumor, shape = Tissue)) +
-#       ggtitle(paste0(gene, "(Pearson's corr = ", round(cor, 2), ")")) +
-#       xlim(0, 1)
-#   }
-# 
-#   if (length(unique(methylation_expression$Tumor)) == 1) {  # color by tissue if only one tumor type
-#     p <- ggplot(methylation_expression[order(methylation_expression$Tissue,
-#                                              decreasing = TRUE), ],
-#                 aes(x = met, y = log1p(TPM))) +
-#       geom_point(alpha = 0.6, aes(color = Tissue)) +
-#       ggtitle(paste0(gene, " in ", tumor, " (corr = ", round(cor, 2), ")")) +
-#       xlim(0, 1)
-#   }
-# 
-# 
-#   if (corr_coeff) {
-#     unname(round(cor, 2))
-#   } else {
-#     if (return == FALSE) {
-#       suppressMessages(print(p))
-#     } else {
-#       methylation_expression
-#     }
-# 
-#   }
-# 
-# }
-
-
-TCGA_methylation_expression_correlation <- function(tumor, gene = NULL,
+#' TCGA_methylation_expression_correlation("LUAD", gene = "TDRD1", 
+#' return = FALSE)
+#' TCGA_methylation_expression_correlation(c("LUAD", "LUSC"), 
+#' gene = "MAGEA1", return = FALSE)
+#' TCGA_methylation_expression_correlation("LUAD", 
+#' gene = "TDRD1", return = TRUE)
+TCGA_methylation_expression_correlation <- function(tumor, 
+                                                    gene = NULL,
+                                                    nt_up = 1000,
+                                                    nt_down = 200,
                                                     return = FALSE,
                                                     corr_coeff = FALSE) {
   
@@ -207,33 +66,30 @@ TCGA_methylation_expression_correlation <- function(tumor, gene = NULL,
   valid_tumor <- c(unique(colData(TPM)$type), "all")
   type <- check_names(variable = tumor, valid_vector = valid_tumor)
   stopifnot("No valid tumor type entered" = length(type) > 0)
-  if (type != "all") {
+  if (!"all" %in% type) {
     TPM <- TPM[, TPM$type %in% type]
     met <- met[, met$type %in% type]
   } 
   
   stopifnot("No valid gene name entered" = !is.null(gene))
   valid_gene_names <- CT_genes$external_gene_name
+  valid_gene_names <- valid_gene_names[valid_gene_names %in% rowData(TPM)$external_gene_name]
   gene <- check_names(gene, valid_gene_names)
+  stopifnot("Gene not in TCGA database" = gene %in% rowData(TPM)$external_gene_name)
+  stopifnot(length(gene) == 1)
   TPM <- TPM[rowData(TPM)$external_gene_name %in% gene, ]
-  
-  
-  
-  
-  
-  # if (!gene %in% CT_genes$external_gene_name) { ## Warning for length(gene) > 1
-  #   message(paste0("Check gene name! ", gene, " is not in the database"))
-  #   return(invisible(NA))
-  # }
   
   ## select tumors for which expression and methylation data are available
   samples <- intersect(colData(TPM)$sample, colData(met)$sample)
+  TPM <- TPM[, TPM$sample %in% samples]
+  met <- met[, met$sample %in% samples]
   
-  ## Create a Grange corresponding to promoter regions
-  nt_up <- 1000
-  nt_down <- 200
-  CT_promoter_gr <- makeGRangesFromDataFrame(
-    CT_genes %>%
+  ## Create a Grange corresponding to promoter region (defined as nt_up 
+  ## nucleotides upstream and nt_down nucleotides downstream TSS)
+  ## Calculates mean methylation value of promoter probe(s) in each sample
+  gene_promoter_gr <- makeGRangesFromDataFrame(
+    CT_genes %>% 
+      dplyr::filter(external_gene_name == gene) %>% 
       dplyr::select(ensembl_gene_id, external_gene_name,
                     external_transcript_name,chromosome_name, strand,
                     transcription_start_site) %>%
@@ -248,50 +104,29 @@ TCGA_methylation_expression_correlation <- function(tumor, gene = NULL,
     start.field = "start",
     end.field = "stop")
   
-  met_roi <- subsetByOverlaps(met[, colData(met)$sample %in% samples],
-                              CT_promoter_gr[CT_promoter_gr$external_gene_name == gene])
-  
-  ## Evaluate mean methylation value of promoter probe(s) in each sample
+  met_roi <- subsetByOverlaps(met, gene_promoter_gr)
   met_mean <- colMeans(assay(met_roi), na.rm = TRUE)
+  
+  ## Keep prefix of TCGA sample names (TCGA-XX-XXXX-XXX) to be able to join
+  ## expression and methylation data
   names(met_mean) <- substr(names(met_mean), 1, 16)
-  
-  ensembl <- CT_genes[CT_genes$external_gene_name == gene, "ensembl_gene_id",
-                      drop = TRUE]
-  TPM <- assay(TPM[rownames(TPM) %in% ensembl,
-                   colData(TPM)$sample %in% samples])
-  
   colnames(TPM) <- substr(colnames(TPM), 1, 16)
   
-  if (nrow(TPM) == 0) {
-    print(paste0(gene, " is not in TCGA expression database"))
-    return(invisible(NA))
-  }
-  
   suppressMessages(
-    methylation_expression <- left_join(enframe(met_mean, name = "sample",
-                                                value = "met"),
-                                        enframe(TPM[1,], name = "sample",
-                                                value = "TPM"))
+    methylation_expression <-   
+      left_join(tibble(sample = names(met_mean), met = met_mean, ),
+                tibble(sample = colnames(TPM), TPM = as.vector(assay(TPM))))
   )
-  
-  suppressMessages(
-    methylation_expression <- methylation_expression %>%
-      left_join(as_tibble(colData(met)) %>%
-                  mutate(Tumor = sub(pattern = "TCGA-", x = project_id,
-                                     replacement = '')) %>%
-                  dplyr::select(sample, Tumor))
-  )
-  
+
   ## stop if no probes or no methylation values for probes within the region
   if (all(is.na(methylation_expression$met))) {
-    message(paste0("No probes for ", gene))
-    return(invisible(NA))
+    message(paste0("No probes or no methylation values for probes for ", gene))
+    cor <- NA
   }
   
   ## Gene has to be expressed (TPM >= 1) in at least 1%
   ## of the samples to evaluate correlation
-  if (length(methylation_expression$TPM[methylation_expression$TPM] >= 1) <
-      ceiling(nrow(methylation_expression) * 0.01)) {
+  if (quantile(methylation_expression$TPM, 0.99) < 1) {
     message(paste0("Too few positive samples to estimate a correlation for ",
                    gene))
     cor <- NA
@@ -300,24 +135,29 @@ TCGA_methylation_expression_correlation <- function(tumor, gene = NULL,
                     log1p(methylation_expression$TPM))$estimate
   }
   
-  TPM <- met <- NULL
+  methylation_expression <- 
+    merge(methylation_expression, 
+          colData(TPM)[, c("sample", "shortLetterCode", "type")])
   
-  methylation_expression$Tissue <- ifelse(substr(methylation_expression$sample,
-                                                 14, 15) == "11",
-                                          "Peritumoral", "Tumor")
+  ## shortLetterCode TP and TM <=> primary / metastatic tumors
+  ## shortLetterCode NT <=> Normal peritumoral tissue 
+  methylation_expression$Tissue <- "Tumor"
+  methylation_expression$Tissue[methylation_expression$shortLetterCode == "NT"] <- "Peritumoral"
   
-  if (length(unique(methylation_expression$Tumor)) > 1) {  # color by tumor type
-    p <- ggplot(methylation_expression[order(methylation_expression$Tissue,
-                                             decreasing = TRUE), ],
+  # Color by tissue (Peritumoral / Tumor) if only one tumor type, 
+  # otherwise color by tumor types.
+  if (length(type) > 1) {  
+    p <- ggplot(
+      as_tibble(methylation_expression[order(methylation_expression$Tissue, 
+                                             decreasing = TRUE), ]),
                 aes(x = met, y = log1p(TPM))) +
-      geom_point(alpha = 0.6, aes(color = Tumor, shape = Tissue)) +
+      geom_point(alpha = 0.6, aes(color = type, shape = Tissue)) +
       ggtitle(paste0(gene, "(Pearson's corr = ", round(cor, 2), ")")) +
       xlim(0, 1)
-  }
-  
-  if (length(unique(methylation_expression$Tumor)) == 1) {  # color by tissue if only one tumor type
-    p <- ggplot(methylation_expression[order(methylation_expression$Tissue,
-                                             decreasing = TRUE), ],
+  } else {  
+    p <- ggplot(
+      as_tibble(methylation_expression[order(methylation_expression$Tissue,
+                                             decreasing = TRUE), ]),
                 aes(x = met, y = log1p(TPM))) +
       geom_point(alpha = 0.6, aes(color = Tissue)) +
       ggtitle(paste0(gene, " in ", tumor, " (corr = ", round(cor, 2), ")")) +
@@ -337,6 +177,4 @@ TCGA_methylation_expression_correlation <- function(tumor, gene = NULL,
   }
   
 }
-
-
 
