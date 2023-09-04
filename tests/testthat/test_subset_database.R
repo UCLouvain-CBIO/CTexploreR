d90eb444853b4c52263149c4f29298df667424f2
Julie Devis <julie.devis@uclouvain.be>
2023-09-04 13:11:20 +0200

    Finished and formatted unit test subset_database

--- tests/testthat/test_subset_database.R
+++ tests/testthat/test_subset_database.R
@@ -1,21 +1,24 @@
 test_that("subset_database() works", {
   dataset <- CTdata::GTEX_data()
   var <- c("MAGEA1", "MAGEA3")
-  res <- CTexploreR:::subset_database(var, dataset)
   
-  # Check two genes gives two genes in dataset
+  ## Returns a Summarized Experiment
+  res <- CTexploreR:::subset_database(var, dataset)
+  expect_s4_class(res, "SummarizedExperiment")
+  
+  ## Check two genes gives two genes in dataset
+  res <- CTexploreR:::subset_database(var, dataset)
   expect_identical(sort(rowData(res)$external_gene_name),
                    sort(var))
   expect_equal(nrow(res), 2)
   
-  # Check no variable gives CT_genes
+  ## Check no variable gives CT_genes
   res_no <- CTexploreR:::subset_database(data = dataset)
   expect_identical(rowData(res_no)$external_gene_name,
-                   CTdata::CT_genes()$external_gene_name)
-  expect_equal(nrow(res_no), nrow(CTdata::CT_genes()))
-
+                   CT_genes$external_gene_name)
+  expect_equal(nrow(res_no), nrow(CT_genes))
   
-  # Check when fake gene name gives nothing + warning
+  ## Check when fake gene name gives nothing + warning
   var_fake <- c("not_existing")
   expect_warning(CTexploreR:::subset_database(var_fake, dataset))
   res_fake <- CTexploreR:::subset_database(var_fake, dataset)
