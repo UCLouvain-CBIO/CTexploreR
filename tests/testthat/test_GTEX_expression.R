559e7df87fdb4ed05f7b065caf13e904918c385d
aloriot <axelle.loriot@uclouvain.be>
2023-09-04 09:25:30 +0200

    added units tests for GTEX_expression

---
+++ tests/testthat/test_GTEX_expression.R
@@ -0,0 +1,33 @@
+test_that("GTEX_expression() works", {
+    
+    ## returns a matrix of double
+    my_genes <- c("MAGEA1", "MAGEA3")
+    res <- CTexploreR:::GTEX_expression(my_genes, return = TRUE)
+    expect_true(inherits(res, "matrix"))
+    expect_type(res, "double")
+    
+    ## n valid genes in input returns a matrix of n expected rownames
+    expect_equal(nrow(res), 2) 
+    expect_identical(sort(rownames(res)), sort(my_genes))
+        
+    my_genes <- "MAGEA1"
+    res <- CTexploreR:::GTEX_expression(my_genes, return = return_val)
+    expect_equal(nrow(res), 1) 
+    
+    my_genes <- ""
+    res <- CTexploreR:::GTEX_expression(my_genes, return = return_val)
+    expect_equal(nrow(res), 0) 
+    expect_warning(CTexploreR:::check_names(my_genes, "MAGEA1"),
+                   "names invalid")
+    
+    ## Test the "log_TPM" units argument
+    res_in_TPM <- CTexploreR:::GTEX_expression("MAGEA1", return = TRUE)
+    res_in_log <- CTexploreR:::GTEX_expression("MAGEA1", 
+                                               units = "log_TPM", return = TRUE)
+    expect_equal(res_in_log[, "Testis"], log1p(res_in_TPM[, "Testis"])) 
+    
+    ## Test that the function returns a heatmap
+    my_genes <- c("MAGEA1", "MAGEA3")
+    res <- CTexploreR:::GTEX_expression(my_genes)
+    expect_s4_class(res, "Heatmap")
+})
